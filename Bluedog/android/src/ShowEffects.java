5b092f05b32e506ace6a0d604b623c069fda5403
Jotha5541 <Jotha5541@gmail.com>
2026-05-24 20:04:10 -0700

    Implementing Bluedog

--- Bluedog/android/src/ShowEffects.java
+++ Bluedog/android/src/ShowEffects.java
@@ -1,23 +1,16 @@
-// ViewController.java
+// Live ViewController.java
 
-package live;
+package bluedog;
 
 import android.graphics.Bitmap;
 
 public interface ShowEffects {
     void flash(double intensity, int durationMs);
-
     void renderImage(Bitmap image, int durationMs);
-
     void playSound(byte[] soundData, int durationMs);
-
     void buzz(int pattern, int durationMs);
-
     void setBackground(Bitmap image);
-
     void showMessage(String message);
-
     void playVideo(String url);
-
     void sync();
 }
